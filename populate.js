ac5ac5c21ab53bf346baa1a76f42a43b9015cf5e
samiullah997 <samiullahk997@gmail.com>
2022-08-02 13:10:04 +0500

    Solve linter error in javascript file and html file

--- populate.js
+++ populate.js
@@ -1,73 +1,61 @@
 let books = [];
 
-if(!localStorage.getItem('ABC')) {
-    localStorage.setItem('ABC', JSON.stringify(books));
+if (!localStorage.getItem('BOOKS')) {
+  localStorage.setItem('BOOKS', JSON.stringify(books));
 } else {
-    books = JSON.parse(localStorage.getItem('ABC'))
+  books = JSON.parse(localStorage.getItem('BOOKS'));
 }
 
-function saveLocally () {
-    localStorage.setItem('ABC', JSON.stringify(books));
-}
-
-const removeBook = (bookToRemove) => {
-    books = books.filter((book) => book !== bookToRemove);
-    saveLocally();
-    render();
-}
-
-const addBook = (bookToAdd) => {
-    books.push(bookToAdd);
-    saveLocally();
-    render();
+function saveLocally() {
+  localStorage.setItem('BOOKS', JSON.stringify(books));
 }
 
 const awesomeBooks = document.createElement('div');
 
 function render() {
+  awesomeBooks.innerHTML = '';
 
-    awesomeBooks.innerHTML = '';
+  const BooknAuthor = (data) => {
+    const container = document.createElement('div');
 
-    const BooknAuthor = (data) => {
+    const bookName = document.createElement('div');
+    bookName.innerText = data.title;
+    container.appendChild(bookName);
 
-        const container = document.createElement('div');
+    const authName = document.createElement('div');
+    authName.innerText = data.author;
+    container.appendChild(authName);
 
-        const bookName = document.createElement('div');
-        bookName.innerText = data.title;
-        container.appendChild(bookName);
-
-        const authName = document.createElement('div');
-        authName.innerText = data.author;
-        container.appendChild(authName);
-
-        const button = document.createElement('button');
-        button.innerText = 'Remove Book';
-        container.appendChild(button);
-        button.onclick = () => {
-            removeBook(data);
-        }
-
-        return container;
+    const button = document.createElement('button');
+    button.innerText = 'Remove Book';
+    container.appendChild(button);
+    button.onclick = () => {
+      books = books.filter((book) => book !== data);
+      saveLocally();
+      render();
     };
 
-    
+    return container;
+  };
 
-    books.forEach((data) => {
-        awesomeBooks.appendChild(BooknAuthor(data));
-    });
+  books.forEach((data) => {
+    awesomeBooks.appendChild(BooknAuthor(data));
+  });
 
-    const section = document.body.querySelector('#awesome-books');
-    section.appendChild(awesomeBooks);
+  const section = document.body.querySelector('#awesome-books');
+  section.appendChild(awesomeBooks);
 }
 
 render();
 
-const form = document.body.querySelector("form");
-form.addEventListener('submit', (event)=>{
-event.preventDefault();
-const title = form.title.value;
-const author = form.author.value;
-const book = {title, author};
-addBook(book);
-form.reset();
-})
+const form = document.body.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const title = form.title.value;
+  const author = form.author.value;
+  const book = { title, author };
+  books.push(book);
+  saveLocally();
+  render();
+  form.reset();
+});
