49ef57cd39043b1edf0c9917be9ffce67984f096
PRATAP-KUMAR <pratap@fastmail.fm>
2022-08-02 12:46:54 +0530

    javascript interactions added

--- populate.js
+++ populate.js
@@ -1,92 +1,73 @@
-import books from './data.js';
+let books = [];
 
-const awesomeBooks = document.createElement('div');
-awesomeBooks.className = 'awesome-books';
-
-const BooknAuthor = (data) => {
-    // Create a Container 
-  const container = document.createElement('div');
-  container.className = 'container';
-
-//   create inner div for book name
-  const bookName = document.createElement('div');
-  bookName.className = 'book-name';
-  bookName.innerText = data.title;
-  container.appendChild(bookName);
-
-//   crea inner div for author name 
-  const authName = document.createElement('div');
-  authName.className = 'auth-name';
-  authName.innerText = data.author;
-  container.appendChild(authName);
-
-    // create Button Remove
-  const btnRemove = document.createElement('button');
-  btnRemove.className = 'btn-remove';
-  btnRemove.innerText = 'Remove';
-  btnRemove.type = 'button';
-  container.appendChild(btnRemove);
-
-//   Create hr for row
-    const hr = document.createElement('hr');
-    container.appendChild(hr);
-
-  return container;
-};
-
-const AddBookForm =()=>{
-    // create Add Book Form 
-    const bookForm = document.createElement('form');
-    bookForm.id ='form';
-    bookForm.action='#';
-    bookForm.method='POST';
-
-    // Create input for Book Name 
-    const bookInput = document.createElement('input');
-    bookInput.name = 'book-name';
-    bookInput.className = 'book-name'
-    bookInput.id = 'book-name';
-    bookInput.type = 'text';
-    bookInput.placeholder = 'Title';
-    bookInput.style.marginTop = ' 10px';
-    bookInput.required = 'true';
-    bookForm.appendChild(bookInput);
-
-    // Create br
-    const br1 = document.createElement('br');
-    bookForm.appendChild(br1);
-
-    // Create Input For Author Name 
-    const authorInput = document.createElement('input');
-    authorInput.name = 'author-name';
-    authorInput.id = 'author-name';
-    authorInput.className = 'author-name';
-    authorInput.type = 'text';
-    authorInput.placeholder = 'Author';
-    authorInput.style.marginTop = ' 10px';
-    authorInput.required = 'true';
-    bookForm.appendChild(authorInput);
-
-    // Create br
-    const br2 = document.createElement('br');
-    bookForm.appendChild(br2);
-
-    // Create Button for Book Add
-    const btnAdd = document.createElement('button');
-    btnAdd.type = 'submit';
-    btnAdd.innerText = 'Add';
-    btnAdd.id = 'btn-add';
-    btnAdd.className = 'btn-add';
-    btnAdd.style.marginTop = '10px';
-    bookForm.appendChild(btnAdd);
-
-    return bookForm
+if(!localStorage.getItem('ABC')) {
+    localStorage.setItem('ABC', JSON.stringify(books));
+} else {
+    books = JSON.parse(localStorage.getItem('ABC'))
 }
 
-books.forEach((data) => {
-  awesomeBooks.appendChild(BooknAuthor(data));
-});
+function saveLocally () {
+    localStorage.setItem('ABC', JSON.stringify(books));
+}
 
-const section = document.body.querySelector('#awesome-books');
-section.appendChild(awesomeBooks);
-section.appendChild(AddBookForm());
+const removeBook = (bookToRemove) => {
+    books = books.filter((book) => book !== bookToRemove);
+    saveLocally();
+    render();
+}
+
+const addBook = (bookToAdd) => {
+    books.push(bookToAdd);
+    saveLocally();
+    render();
+}
+
+const awesomeBooks = document.createElement('div');
+
+function render() {
+
+    awesomeBooks.innerHTML = '';
+
+    const BooknAuthor = (data) => {
+
+        const container = document.createElement('div');
+
+        const bookName = document.createElement('div');
+        bookName.innerText = data.title;
+        container.appendChild(bookName);
+
+        const authName = document.createElement('div');
+        authName.innerText = data.author;
+        container.appendChild(authName);
+
+        const button = document.createElement('button');
+        button.innerText = 'Remove Book';
+        container.appendChild(button);
+        button.onclick = () => {
+            removeBook(data);
+        }
+
+        return container;
+    };
+
+    
+
+    books.forEach((data) => {
+        awesomeBooks.appendChild(BooknAuthor(data));
+    });
+
+    const section = document.body.querySelector('#awesome-books');
+    section.appendChild(awesomeBooks);
+}
+
+render();
+
+const form = document.body.querySelector("form");
+form.addEventListener('submit', (event)=>{
+event.preventDefault();
+const title = form.title.value;
+const author = form.author.value;
+const book = {title, author};
+addBook(book);
+form.reset();
+})
